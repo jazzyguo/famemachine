460c2ebf145d86de33499c7fc25111746d933001
Jazzy <guojazzy@gmail.com>
2023-05-31 09:59:37 +0200

    fix: videoidcontainer move prop out

--- pages/videos/twitch/[video_id].tsx
+++ pages/videos/twitch/[video_id].tsx
@@ -1,11 +1,24 @@
+import { useRouter } from "next/router";
 import PublishModalModule from "@/modules/Publish/PublishModal";
 import VideoIDModule from "@/modules/Twitch/VideoID";
 import withAuth from "@/components/hoc/withAuth";
 
-const TwitchVideoPage = () =>
-    <>
-        <PublishModalModule />
-        <VideoIDModule />
-    </>
+const TwitchVideoPage = () => {
+    const router = useRouter();
+    const { video_id } = router.query;
+
+    const videoId =
+        video_id && (typeof video_id === 'string' ? video_id : video_id[0]) || ''
+
+    return (
+        <>
+            <PublishModalModule />
+            <VideoIDModule
+                videoId={videoId}
+            />
+        </>
+    )
+}
+
 
 export default withAuth(TwitchVideoPage);
