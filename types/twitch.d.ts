6050b39c19389c112becdca8cc71f88ee6d44dd2
Jazzy Guo <guojazzy@gmail.com>
2023-07-09 17:15:04 -0400

    fix: remove unused twitch type

--- types/twitch.d.ts
+++ types/twitch.d.ts
@@ -13,27 +13,3 @@ type RefreshTokenActionProps = {
     refreshToken: string;
     addConnection: (connection: string, obj: { [key: string]: string }) => void;
 };
-
-type Actions = {
-    fetchTwitchVideos: ({
-        twitchUserId,
-        twitchAccessToken,
-        addConnection,
-        userId,
-        refreshToken,
-    }: RefreshTokenActionProps & {
-        twitchUserId: string;
-        twitchAccessToken: string;
-    }) => void;
-    fetchTwitchVideo: ({
-        twitchAccessToken,
-        videoId,
-        addConnection,
-        userId,
-        refreshToken,
-    }: RefreshTokenActionProps & {
-        twitchAccessToken: string;
-        videoId: string;
-    }) => void;
-    reset: () => void;
-};
