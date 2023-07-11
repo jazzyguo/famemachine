2bf390883799fdc1258f3cc53b4ac7b0fa9e7ffc
Jazzy Guo <guojazzy@gmail.com>
2023-07-11 18:42:16 -0400

    feat: add websocket

--- api/clips/processTwitchVod.ts
+++ api/clips/processTwitchVod.ts
@@ -1,19 +1,29 @@
-import { useMutation } from '@tanstack/react-query';
+import { useEffect } from "react";
+import { useMutation } from "@tanstack/react-query";
+import io from "socket.io-client";
+import axios from "@/lib/axios";
+import { MutationConfig, queryClient } from "@/lib/react-query";
+import { ATHENA_API_URL } from "@/lib/consts/api";
 
-import axios from '@/lib/axios';
-import { MutationConfig, queryClient } from '@/lib/react-query';
+const socket = io(ATHENA_API_URL);
 
 type ProcessTwitchVodDTO = {
     timestamp: [number, number];
-    videoId: string
+    videoId: string;
 };
 
-const processTwitchVod = async ({ timestamp, videoId }: ProcessTwitchVodDTO): Promise<TempClip[]> => {
-    const response: { clips: TempClip[] } = await axios.post(`/twitch/process_vod/${videoId}`, {
-        start: timestamp[0],
-        end: timestamp[1],
-    });
-    return response.clips
+const processTwitchVod = async ({
+    timestamp,
+    videoId,
+}: ProcessTwitchVodDTO): Promise<TempClip[]> => {
+    const response: { clips: TempClip[] } = await axios.post(
+        `/twitch/process_vod/${videoId}`,
+        {
+            start: timestamp[0],
+            end: timestamp[1],
+        }
+    );
+    return response.clips;
 };
 
 type useProcessTwitchVodOptions = {
@@ -21,18 +31,35 @@ type useProcessTwitchVodOptions = {
 };
 
 const useProcessTwitchVod = ({ config }: useProcessTwitchVodOptions = {}) => {
+    useEffect(() => {
+        // Listen for the 'twitch_vod_processed' event from the WebSocket server
+        socket.on("twitch_vod_processed", (response_data) => {
+            // Handle the received data
+            // You can update the state or perform any necessary actions based on the data
+            console.log("Received processed Twitch VOD:", response_data);
+        });
+
+        return () => {
+            // Clean up the WebSocket connection when the component unmounts
+            socket.off("twitch_vod_processed");
+        };
+    }, []);
+
     return useMutation<TempClip[], any, ProcessTwitchVodDTO>({
         onSuccess: (newTempClips) => {
             // Update temporary clips data by pushing the new generated clips if not existing
-            queryClient.setQueryData<TempClip[] | null>(['temporaryClips'], (data) => {
-                if (data) {
-                    return [...data, ...newTempClips];
+            queryClient.setQueryData<TempClip[] | null>(
+                ["temporaryClips"],
+                (data) => {
+                    if (data) {
+                        return [...data, ...newTempClips];
+                    }
                 }
-            });
+            );
         },
         ...config,
         mutationFn: processTwitchVod,
     });
 };
 
-export default useProcessTwitchVod
+export default useProcessTwitchVod;
